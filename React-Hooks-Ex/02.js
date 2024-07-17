1edb2c8ea19e59cbd78270ef401a36015dafe292
Dylan <dylan.drumgoole@fenergo.com>
2024-07-17 15:06:36 +0100

    Extra credit 2
    Adding dependency array to useEffect to only re render depending on changes to "name"

--- React-Hooks-Ex/02.js
+++ React-Hooks-Ex/02.js
@@ -14,7 +14,7 @@ function Greeting({initialName = ''}) {
 
   React.useEffect(() => {
     window.localStorage.setItem('name', name)
-  })
+  }, [name])
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
