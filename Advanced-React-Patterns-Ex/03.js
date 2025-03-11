c42a1debcc542d24b4ed5f1756b291008845ea81
Dylan <dylan.drumgoole@fenergo.com>
2025-03-11 16:09:04 +0000

    ex 3

---
+++ Advanced-React-Patterns-Ex/03.js
@@ -0,0 +1,71 @@
+// Flexible Compound Components
+// http://localhost:3000/isolated/exercise/03.js
+
+import * as React from 'react'
+import {Switch} from '../switch'
+
+// 🐨 create your ToggleContext context here
+// 📜 https://react.dev/reference/react/createContext
+
+const ToggleContext = React.createContext()
+ToggleContext.displayName = 'ToggleContext'
+
+function Toggle({children}) {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+
+  // 🐨 remove all this 💣 and instead return <ToggleContext.Provider> where
+  // the value is an object that has `on` and `toggle` on it.
+  return (
+    <ToggleContext.Provider value={{on, toggle}}>
+      {children}
+    </ToggleContext.Provider>
+  )
+}
+
+// 🐨 we'll still get the children from props (as it's passed to us by the
+// developers using our component), but we'll get `on` implicitly from
+// ToggleContext now
+// 🦉 You can create a helper method to retrieve the context here. Thanks to that,
+// your context won't be exposed to the user
+// 💰 `const context = React.useContext(ToggleContext)`
+// 📜 https://react.dev/reference/react/useContext
+function useToggle() {
+  return React.useContext(ToggleContext)
+}
+
+function ToggleOn({children}) {
+  const {on} = useToggle()
+  return on ? children : null
+}
+
+function ToggleOff({children}) {
+  const {on} = useToggle()
+  return on ? null : children
+}
+
+function ToggleButton({...props}) {
+  const {on, toggle} = useToggle()
+  return <Switch on={on} onClick={toggle} {...props} />
+}
+
+function App() {
+  return (
+    <div>
+      <Toggle>
+        <ToggleOn>The button is on</ToggleOn>
+        <ToggleOff>The button is off</ToggleOff>
+        <div>
+          <ToggleButton />
+        </div>
+      </Toggle>
+    </div>
+  )
+}
+
+export default App
+
+/*
+eslint
+  no-unused-vars: "off",
+*/
