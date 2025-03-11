49b903e6be11805a9ab0ae74157c858fc16b8958
Dylan <dylan.drumgoole@fenergo.com>
2025-03-11 15:11:35 +0000

    ex 2

---
+++ Advanced-React-Patterns-Ex/02.js
@@ -0,0 +1,53 @@
+// Compound Components
+// http://localhost:3000/isolated/exercise/02.js
+
+import * as React from 'react'
+import {Switch} from '../switch'
+
+function Toggle() {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+
+  // 🐨 replace this with a call to React.Children.map and map each child in
+  // props.children to a clone of that child with the props they need using
+  // React.cloneElement.
+  // 💰 React.Children.map(props.children, child => {/* return child clone here */})
+  // 📜 https://react.dev/reference/react/Children
+  // 📜 https://react.dev/reference/react/cloneElement
+
+  return React.Children.map(children, child => {
+    return React.cloneElement(child, {on, toggle})
+  })
+}
+
+// 🐨 Flesh out each of these components
+
+// Accepts `on` and `children` props and returns `children` if `on` is true
+const ToggleOn = ({on, children}) => (on ? children : null)
+
+// Accepts `on` and `children` props and returns `children` if `on` is false
+const ToggleOff = ({on, children}) => (on ? null : children)
+
+// Accepts `on` and `toggle` props and returns the <Switch /> with those props.
+const ToggleButton = ({on, toggle, ...props}) => {
+  return <Switch on={on} onClick={toggle} {...props} />
+}
+
+function App() {
+  return (
+    <div>
+      <Toggle>
+        <ToggleOn>The button is on</ToggleOn>
+        <ToggleOff>The button is off</ToggleOff>
+        <ToggleButton />
+      </Toggle>
+    </div>
+  )
+}
+
+export default App
+
+/*
+eslint
+  no-unused-vars: "off",
+*/
